fe14d9958f0ca0c22bd6115bee1af7de64f57905
Dev Kordes <dev.kordes@gmail.com>
2018-03-08 15:16:53 -0500

    Minor react setup changes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,7 +1,6 @@
-/**
- * Next, we will create a fresh React component instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import ReactDOM from 'react-dom';
+import React from 'react';
 
-require('./components/Example');
+import App from './components/App';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
