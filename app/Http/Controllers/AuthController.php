c1ef4afad89412605cf83bb1902cb510bcfb5f76
Dev Kordes <dev.kordes@gmail.com>
2018-03-10 13:01:37 -0500

    Register alpha implemented

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+
+class AuthController extends Controller
+{
+    // User model
+    protected $user;
+
+    protected $hash;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+        $this->hash = app()->make('hash');
+        $this->auth = app()->make('auth');
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $data = $request->input();
+        $data['password'] = $this->hash->make($data['password']);
+
+        $user = $this->user->create($data);
+
+        $token = $this->auth->attempt($request->only([
+            'email', 'password',
+        ]));
+
+        return response()->json(array_merge([
+            'user' => $user
+        ], [
+            'token' => $this->tokenArray($token)
+        ]));
+    }
+
+    public function login(LoginRequest $request)
+    {
+
+    }
+
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return array
+     */
+    protected function tokenArray($token)
+    {
+        return [
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ];
+    }
+}
