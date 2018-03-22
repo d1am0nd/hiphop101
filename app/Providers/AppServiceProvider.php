89a4e56ed4997d71a8d011d0fc9e9621ba68460f
Dev Kordes <dev.kordes@gmail.com>
2018-03-22 16:09:32 -0400

    Minor design changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
         \DB::listen(function ($query) {
             \Log::info([
                 $query->sql,
@@ -21,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
                 $query->time
             ]);
         });
+        /*
         */
     }
 
