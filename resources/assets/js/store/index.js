f5fad35f5fa9479d54794f6274701b50df9ebfe8
Dev Kordes <dev.kordes@gmail.com>
2018-04-02 02:46:28 -0400

    Minor changes

--- resources/assets/js/store/index.js
+++ resources/assets/js/store/index.js
@@ -5,7 +5,6 @@ import {
 } from 'redux';
 import thunk from 'redux-thunk';
 
-import {miscReducer} from '@/store/reducers/misc';
 import {authReducer} from '@/store/reducers/auth';
 import {modalReducer} from '@/store/reducers/modal';
 import {artistReducer} from '@/store/reducers/artists';
